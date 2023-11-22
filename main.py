e0475c902e7e8420bac31bc0c325f7dcf8f467c5
AyrtonCossuol <ayrton.cossuol@live.com>
2023-11-21 21:34:38 -0300

    add library and hard code send data for topic in Kafka

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import time
+from confluent_kafka import Producer
+import socket
 
 from src.generate_fake_data.main import FakeGenerate
 
 
 def main() -> None:
     fake = FakeGenerate()
-    aux = 5
-    while aux !=0:
-        print(fake.generate_data_fake())
-        time.sleep(3)
-        aux -=1
+    # aux = 5
+    # while aux !=0:
+    #     print(fake.generate_data_fake())
+    #     time.sleep(3)
+    #     aux -=1
+
+
+
+    conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'client.id': socket.gethostname()
+    }
+
+    producer = Producer(conf)
+    topic = 'demo-test-1'
+
+    producer.produce(topic, key="key", value=fake.generate_data_fake())
+    producer.flush()
 
 if __name__ == "__main__":
     main()
